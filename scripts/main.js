b30a659f27bff14b53190b1c4f3b67d82491114f
Kerr Beeldens <kerrbeeldens@outlook.com>
2026-03-24 16:53:35 +0100

    Implement student showcase
    - TODO: consider responsiveness

---
+++ scripts/main.js
@@ -0,0 +1,107 @@
+const svg = document.querySelector("svg");
+const paths = Array.from(svg.querySelectorAll("path")).slice(0, 8);
+const container = document.querySelector(".dots");
+
+const starsPerPath = 1;
+const duration = 2000; // in ms
+
+paths.forEach((path, k) => {
+  const d = path.getAttribute("d");
+
+  for (let i = 0; i < starsPerPath; i++) {
+    const star = document.createElement("div");
+    star.className = "star";
+
+    for (let j = 0; j < 20; j++) {
+      const dot = document.createElement("div");
+      dot.className = "dot";
+
+      const size = 10 - j * 0.4; // taper from 10px to 2px
+
+      dot.style.width = `${size}px`;
+      dot.style.height = `${size}px`;
+      dot.style.offsetPath = `path('${d}')`;
+
+      /*  The delay depends on:
+      - i: each spiral has equally spaced stars
+      - j: each star has a number of trailing dots with a small delay
+      - k: each spiral is slightly out of face with the others
+      */
+      const delay =
+        ((i / starsPerPath) * duration) / 1000 + j * 0.0075 + k * 0.25;
+
+      dot.style.setProperty("--delay", `${delay}s`);
+      dot.style.setProperty("--duration", `${duration}ms`);
+
+      star.appendChild(dot);
+    }
+
+    container.append(star);
+  }
+});
+
+// Students
+const studentsPerPath = 1;
+const studentDuration = 5000; // in ms
+
+paths.forEach((path, k) => {
+  const d = path.getAttribute("d");
+
+  if (k % 2 === 0) return;
+
+  for (let i = 0; i < studentsPerPath; i++) {
+    const student = document.createElement("div");
+    student.className = "student";
+
+    for (let j = 0; j < 40; j++) {
+      const dot = document.createElement("div");
+      dot.className = "dot";
+
+      const size = 40 - j * 0.8; // taper from 10px to 2px
+
+      dot.style.width = `${size}px`;
+      dot.style.height = `${size}px`;
+      dot.style.offsetPath = `path('${d}')`;
+
+      /*  The delay depends on:
+      - i: each spiral has equally spaced stars
+      - j: each star has a number of trailing dots with a small delay
+      - k: each spiral is slightly out of face with the others
+      */
+      const delay =
+        ((i / (starsPerPath + 2)) * duration) / 1000 + j * 0.0075 + k * 0.37;
+
+      dot.style.setProperty("--delay", `${delay}s`);
+      dot.style.setProperty("--duration", `${studentDuration}ms`);
+
+      student.appendChild(dot);
+    }
+
+    container.append(student);
+  }
+});
+
+const studentImages = [
+  ["student1.jpg", "Yoda"],
+  ["student2.jpg", "Spock"],
+  ["student3.jpg", "E.T"],
+  ["student4.jpeg", "Elon Musk"],
+];
+
+function getRandomStudent() {
+  return studentImages[Math.floor(Math.random() * studentImages.length)];
+}
+
+document.querySelectorAll(".student .dot:first-of-type").forEach((dot) => {
+  const student = getRandomStudent();
+
+  dot.style.backgroundImage = `url(../resources/${student[0]})`;
+  dot.style.setProperty("--label", `"${student[1]}"`);
+
+  dot.addEventListener("animationiteration", () => {
+    const student = getRandomStudent();
+
+    dot.style.backgroundImage = `url(../resources/${student[0]})`;
+    dot.style.setProperty("--label", `"${student[1]}"`);
+  });
+});
